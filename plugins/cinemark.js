eef99e04b2bda7eb04b03dc40c9059439bba71f4
lndgalante <lndgalante@gmail.com>
2019-06-27 18:15:56 -0300

    Add plugins folder

---
+++ plugins/cinemark.js
@@ -0,0 +1,19 @@
+const fp = require('fastify-plugin')
+const cron = require('node-cron')
+
+const { getCinemarkData } = require('../utils')
+
+module.exports = fp(function(fastify, opts, next) {
+  const saveCinemarkDataToRedis = async () => {
+    const { data, error } = await getCinemarkData()
+    if (error) fastify.log.error(error)
+    if (data) data.forEach(([key, value]) => fastify.redis.set(key, JSON.stringify(value)))
+  }
+
+  fastify.decorate('getCinemarkData', async () => {
+    saveCinemarkDataToRedis()
+    cron.schedule('*/5 * * * *', () => saveCinemarkDataToRedis())
+  })
+
+  next()
+})
--- server.js
+++ server.js
@@ -1,37 +1,27 @@
-const Fastify = require('fastify')
-const AutoLoad = require('fastify-autoload')
-const fastifyRedis = require('fastify-redis')
-const cron = require('node-cron')
-const dotenv = require('dotenv')
 const path = require('path')
-
-const { getCinemarkData } = require('./utils')
+const dotenv = require('dotenv')
+const Fastify = require('fastify')
+const autoload = require('fastify-autoload')
+const fastifyRedis = require('fastify-redis')
 
 // Load enviroment variables
 dotenv.config()
 
-// Create a fastify instance. Register Redis and controllers.
+// Create a fastify instance
 const fastify = Fastify({ logger: true })
+
+// Register redis, plugins and controllers
 fastify.register(fastifyRedis, { host: '127.0.0.1' })
-fastify.register(AutoLoad, { dir: path.join(__dirname, 'controllers') })
-
-// Get cinemark data and save it to Redis.
-const saveCinemarkDataToRedis = async () => {
-  const { data, error } = await getCinemarkData()
-  if (error) console.error(error)
-  if (data) data.forEach(([key, value]) => fastify.redis.set(key, JSON.stringify(value)))
-}
-
-// Get cinemark data at the server start. Run it a cron every five minutes.
-saveCinemarkDataToRedis()
-cron.schedule('*/5 * * * *', () => saveCinemarkDataToRedis())
+fastify.register(autoload, { dir: path.join(__dirname, 'plugins') })
+fastify.register(autoload, { dir: path.join(__dirname, 'controllers') })
 
 // Main function
 const main = async () => {
   try {
     await fastify.listen(process.env.PORT || 3000)
-    fastify.log.info(`server listening on ${fastify.server.address().port}`)
-  } catch (err) {
+    const { port } = fastify.server.address()
+    fastify.log.info(`Server running on ${port}`)
+  } catch (error) {
     fastify.log.error(err)
     process.exit(1)
   }
