0294c446ca05a36ec7b73981afe14b808c5d339d
lndgalante <lndgalante@gmail.com>
2019-06-30 15:26:55 -0300

    Improve shows structure

--- utils/parsers/shows.js
+++ utils/parsers/shows.js
@@ -1,47 +1,35 @@
-const titleize = require("titleize");
-const dayjs = require("dayjs");
+const titleize = require('titleize')
+const dayjs = require('dayjs')
+require('dayjs/locale/es')
 
-require("dayjs/locale/es");
-
-dayjs.locale("es");
+dayjs.locale('es')
 
 const createTicketsLink = (cinemaId, sessionId, featureId) => {
-  const baseUrl = "https://tickets.cinemarkhoyts.com.ar/NSTicketing";
-  return `${baseUrl}/?CinemaId=${cinemaId}&SessionId=${sessionId}&FeatureId=${featureId}`;
-};
+  const baseUrl = 'https://tickets.cinemarkhoyts.com.ar/NSTicketing'
+  return `${baseUrl}/?CinemaId=${cinemaId}&SessionId=${sessionId}&FeatureId=${featureId}`
+}
 
 const parseShows = movieList => {
-  const showsParsed = movieList.map(
-    ({id: formatId, format, version, cinemaList}) => {
-      const cinemaWithShows = cinemaList.reduce(
-        (acc, {id: cinemaId, sessionList}) => {
-          const shows = sessionList.map(
-            ({id: sessionId, feature: featureId, dtm: timestamp, seats}) => ({
-              seats,
-              timestamp,
-              date: titleize(dayjs(timestamp).format("dddd D [de] MMMM")),
-              time: dayjs(timestamp).format("HH[:]mm"),
-              link: createTicketsLink(cinemaId, sessionId, featureId),
-            })
-          );
-
-          acc[cinemaId] = shows;
-
-          return acc;
-        },
-        {}
-      );
-
-      return {
-        formatId,
+  const showsParsed = movieList.map(({ id: formatId, format, version, cinemaList }) => {
+    const cinemaWithShows = cinemaList.map(({ id: cinemaId, sessionList }) => {
+      const shows = sessionList.map(({ id: sessionId, feature: featureId, dtm: timestamp, seats }) => ({
+        seats,
+        cinemaId: String(cinemaId),
+        formatId: String(formatId),
         format: titleize(format),
         version: titleize(version),
-        cinemaWithShows,
-      };
-    }
-  );
+        time: dayjs(timestamp).format('HH[:]mm'),
+        date: titleize(dayjs(timestamp).format('dddd D [de] MMMM')),
+        link: createTicketsLink(cinemaId, sessionId, featureId),
+      }))
 
-  return showsParsed;
-};
+      return shows
+    })
 
-module.exports = {parseShows};
+    return cinemaWithShows
+  })
+
+  return showsParsed.flat(2)
+}
+
+module.exports = { parseShows }
