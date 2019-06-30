a67dbe29e8ddf2b1415b5fbccbbb0eab2d3c9649
lndgalante <lndgalante@gmail.com>
2019-06-30 18:16:36 -0300

    Add Helmet and Compress

--- client.js
+++ client.js
@@ -1,7 +1,14 @@
 const Fastify = require('fastify');
 const fastifyRedis = require('fastify-redis');
+const fastifyHelmet = require('fastify-helmet');
+const fastifyCompress = require('fastify-compress');
 
+// Instatiate fastify
 const client = Fastify({ logger: true });
+
+// Register plugins
+client.register(fastifyHelmet);
+client.register(fastifyCompress);
 client.register(fastifyRedis, { host: '127.0.0.1' });
 
 module.exports = client;
