e7ecba025ca845b756b197c2493e3c80c08961e0
hdf1986 <hugo.farji+github@gmail.com>
2019-07-01 18:18:50 -0300

    Testing redis config

--- client.js
+++ client.js
@@ -9,6 +9,6 @@ const client = Fastify({ logger: true });
 // Register plugins
 client.register(fastifyHelmet);
 client.register(fastifyCompress);
-client.register(fastifyRedis, process.env['REDISTOGO_URL'] || { host: '127.0.0.1' });
+client.register(fastifyRedis, (process.env.REDIS_URL || (process.env.REDIS_PROVIDER && process.env[process.env.REDIS_PROVIDER]) || { host: '127.0.0.1' }));
 
 module.exports = client;
