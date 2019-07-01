17d1978b43f7ba3229d8cdc5cbd4bd091fc4dd60
hdf1986 <hugo.farji+github@gmail.com>
2019-07-01 18:41:59 -0300

    Removed logs

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ client.get('/shows/:movieId/:cinemaId', getShowsByMovieIdAndCinemaId);
 const main = async () => {
   try {
     await client.listen(process.env.PORT || 3000);
-    console.log(process.env.PORT);
     client.log.info(`Server running on ${client.server.address()}`);
   } catch (error) {
     client.log.error();
