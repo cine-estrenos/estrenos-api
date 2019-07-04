ba25468e64766317baccf3cfbf665fa60790a375
lndgalante <lndgalante@gmail.com>
2019-07-04 16:53:54 -0300

    Add cors

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 import Fastify from 'fastify';
+import fastifyCors from 'fastify-cors';
 import fastifyRedis from 'fastify-redis';
 import fastifyHelmet from 'fastify-helmet';
 import fastifyCompress from 'fastify-compress';
@@ -11,6 +12,7 @@ const redisOptions = process.env.REDIS_URL ||
   (process.env.REDIS_PROVIDER && process.env[process.env.REDIS_PROVIDER]) || { host: '127.0.0.1' };
 
 // Register plugins
+client.register(fastifyCors);
 client.register(fastifyHelmet);
 client.register(fastifyCompress);
 client.register(fastifyRedis, redisOptions);
