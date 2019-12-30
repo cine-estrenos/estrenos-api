5914f6a223a8e8038c858bcfa6b229a598e98cd4
Leonardo Galante <lndgalante@gmail.com>
2019-12-30 13:35:26 -0300

    Remove logs

--- utils/parsers/movies.js
+++ utils/parsers/movies.js
@@ -85,10 +85,8 @@ const parseMovies = async (movies) => {
     parsedMovies.map(async (movie) => {
       try {
         const imdbInfo = await getImdbInfo(movie.title);
-        console.log('TCL: parseMovies -> imdbInfo', imdbInfo);
         return { ...movie, ...imdbInfo };
       } catch (error) {
-        console.log('TCL: parseMovies -> error', error);
         return { ...movie, votes: '0', backdrop: '' };
       }
     }),
