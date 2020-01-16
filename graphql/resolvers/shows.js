0bb249ba5384b3a4cf84c19e1a12acee41f630b6
Leonardo Galante <lndgalante@gmail.com>
2020-01-16 15:12:15 -0300

    Add try/catch to shows resolver

--- graphql/resolvers/shows.js
+++ graphql/resolvers/shows.js
@@ -3,17 +3,22 @@ import { getShowsByMovieIdResolver, getShowsByMovieIdAndCinemaIdResolver } from 
 export default {
   Query: {
     shows: async (_, { movieId, cinemaId }) => {
-      if (movieId && cinemaId) {
-        const showsInCinema = await getShowsByMovieIdAndCinemaIdResolver(movieId, cinemaId);
-        return showsInCinema;
-      }
+      try {
+        if (movieId && cinemaId) {
+          const showsInCinema = await getShowsByMovieIdAndCinemaIdResolver(movieId, cinemaId);
+          return showsInCinema;
+        }
 
-      if (movieId) {
-        const showsByMovie = await getShowsByMovieIdResolver(movieId);
-        return showsByMovie;
-      }
+        if (movieId) {
+          const showsByMovie = await getShowsByMovieIdResolver(movieId);
+          return showsByMovie;
+        }
 
-      return [];
+        return [];
+      } catch (error) {
+        console.log('Error on shows resolver', error); // eslint-disable-line no-console
+        return [];
+      }
     },
   },
 };
