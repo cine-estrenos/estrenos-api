141e119d9fc4d12066f414837c60c49244b861af
Leonardo Galante <lndgalante@gmail.com>
2020-01-15 23:51:21 -0300

    Add introspection flag

--- plugins/apollo.js
+++ plugins/apollo.js
@@ -4,6 +4,6 @@ import typeDefs from '../graphql/types';
 import resolvers from '../graphql/resolvers';
 
 const { ApolloServer } = ApolloServerFastify;
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers, introspection: true });
 
 export default server.createHandler();
